be543429542cce426e1ec70c2452a489d98dd34d
martinbaumgaertner <40661076+martinbaumgaertner@users.noreply.github.com>
2021-10-29 19:55:37 +0200

    new design

--- app.R
+++ app.R
@@ -21,39 +21,60 @@ umap_data<-readRDS(paste0("data/umap_data.Rds"))
 #   name = "dark_theme"
 # )
 
-ui <- dashboardPage(
-    #fullscreen = TRUE,
-    header=dashboardHeader(
-      tags$style(HTML('.js-irs-0 .irs-bar {border-top-color: #80ba24;border-bottom-color: #80ba24;} 
-                        .js-irs-0 .irs-bar-edge {border-color: #80ba24;}
-                        .js-irs-0 .irs-single, .js-irs-0 .irs-bar-edge, .js-irs-0 .irs-bar, .js-irs-0 .irs-from, .js-irs-0 .irs-to {background: #80ba24;}
-                        /* body */.content-wrapper, .right-side {background-color: #ffffff;}
-                        box-body {padding-top: 0px;}
-                      '))),
-    dashboardSidebar(disable = T),
-    dashboardBody(
-        skin = "light",
-                    box(title="UMAP decomposition of documents",
-                        width=12,
-                        height="90vh",
-                        overflow = F,
-                        ggvisOutput("umap_plot"),
-                        sidebar=boxSidebar(
-                          skin = "light",
-                          width = 25,
-                          tabName = "Filter",
-                          id="Filter",
-                          sliderInput("year", "Year released", 1977, 2021, value = c(1977, 2021),sep = ""),
-                          selectInput("type", "Type",c("All",unique(umap_data$type))),
-                          selectInput("cb", "central bank",c("All",unique(umap_data$cb))),
-                          selectInput("currency", "currency",c("All",unique(umap_data$currency))),
-                          textInput("speaker", "Speaker name",placeholder = "e.g. Mario Draghi"),
-                          selectInput("color", "Color",c("type","cb","currency","speaker","year"))
-                        ),
-                        p("UMAP decomposition based on: McInnes, L., Healy, J., & Melville, J. (2018). Umap: Uniform manifold approximation and projection for dimension reduction. arXiv preprint arXiv:1802.03426.")
-                     )
-        )
-    )
+ui<-fluidPage(
+tags$style(HTML('.js-irs-0 .irs-bar {border-top-color: #80ba24;border-bottom-color: #80ba24;}
+                            .js-irs-0 .irs-bar-edge {border-color: #80ba24;}
+                            .js-irs-0 .irs-single, .js-irs-0 .irs-bar-edge, .js-irs-0 .irs-bar, .js-irs-0 .irs-from, .js-irs-0 .irs-to {background: #80ba24;}
+                            /* body */.content-wrapper, .right-side {background-color: #ffffff;}
+                            box-body {padding-top: 0px;}
+                          ')),
+  sidebarLayout(
+    sidebarPanel(
+      width = 2,
+    sliderInput("year", "Year released", 1977, 2021, value = c(1977, 2021),sep = ""),
+                                            selectInput("type", "Type",c("All",unique(umap_data$type))),
+                                            selectInput("cb", "Central Bank",c("All",unique(umap_data$cb))),
+                                            selectInput("currency", "Currency",c("All",unique(umap_data$currency))),
+                                            textInput("speaker", "Speaker name",placeholder = "e.g. Mario Draghi"),
+                                            selectInput("color", "Color",c("type","cb","currency","speaker","year"))
+    ),
+  
+    mainPanel(ggvisOutput("umap_plot"))
+  )
+)
+
+# ui <- dashboardPage(
+#     #fullscreen = TRUE,
+#     header=dashboardHeader(
+#       tags$style(HTML('.js-irs-0 .irs-bar {border-top-color: #80ba24;border-bottom-color: #80ba24;} 
+#                         .js-irs-0 .irs-bar-edge {border-color: #80ba24;}
+#                         .js-irs-0 .irs-single, .js-irs-0 .irs-bar-edge, .js-irs-0 .irs-bar, .js-irs-0 .irs-from, .js-irs-0 .irs-to {background: #80ba24;}
+#                         /* body */.content-wrapper, .right-side {background-color: #ffffff;}
+#                         box-body {padding-top: 0px;}
+#                       '))),
+#     dashboardSidebar(disable = T),
+#     dashboardBody(
+#         skin = "light",
+#                     box(title="UMAP decomposition of documents",
+#                         width=12,
+#                         height="90vh",
+#                         overflow = F,
+#                         ggvisOutput("umap_plot"),
+#                         sidebar=boxSidebar(
+#                           skin = "light",
+#                           width = 25,
+#                           tabName = "Filter",
+#                           id="Filter",
+#                           sliderInput("year", "Year released", 1977, 2021, value = c(1977, 2021),sep = ""),
+#                           selectInput("type", "Type",c("All",unique(umap_data$type))),
+#                           selectInput("cb", "central bank",c("All",unique(umap_data$cb))),
+#                           selectInput("currency", "currency",c("All",unique(umap_data$currency))),
+#                           textInput("speaker", "Speaker name",placeholder = "e.g. Mario Draghi"),
+#                           selectInput("color", "Color",c("type","cb","currency","speaker","year"))
+#                         )
+#                      )
+#         )
+#     )
 
 server <- function(input, output) {
     tooltip<-function(x){
